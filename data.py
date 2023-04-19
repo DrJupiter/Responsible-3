093d3dbde809314f8f5fff3c66ca34f817c0641a
DrJupiter <klausjupiter@hotmail.com>
2023-04-19 10:47:21 +0200

    initial commit additional files and clean up is needed

---
+++ data.py
@@ -0,0 +1,66 @@
+import numpy as np
+from torch.utils import data
+import torchvision.transforms as transforms
+from torch import flatten as t_flatten
+from torchvision.datasets import MNIST, CIFAR10
+import multiprocessing as mp
+
+import jax.numpy as jnp
+
+def numpy_collate(batch):
+  """
+  Collation function for getting samples
+  from `NumpyLoader`
+  """
+  if isinstance(batch[0], np.ndarray):
+    return np.stack(batch)
+  elif isinstance(batch[0], (tuple,list)):
+    transposed = zip(*batch)
+    return [numpy_collate(samples) for samples in transposed]
+  else:
+    return np.array(batch)
+
+class NumpyLoader(data.DataLoader):
+  """
+  The dataloader used for our image datasets
+  """
+  # TODO: Work with data in torch tensor until we pass it to model. As this loader is made to work on those, and is slow and has problems if not done like this.
+  def __init__(self, dataset, batch_size=1,
+                shuffle=False, sampler=None,
+                batch_sampler=None, num_workers=0,
+                pin_memory=False, drop_last=False,
+                timeout=0, worker_init_fn=None):
+    super(self.__class__, self).__init__(dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        sampler=sampler,
+        batch_sampler=batch_sampler,
+        num_workers=num_workers,
+        collate_fn=numpy_collate,
+        pin_memory=pin_memory,
+        drop_last=drop_last,
+        timeout=timeout,
+        worker_init_fn=worker_init_fn)
+      
+class FlattenAndCast(object):
+  """
+  Flattens an image and converts the datatype to be 
+  jax numpy's float32 type
+  """
+  def __call__(self, pic):
+    return np.ravel(np.array(pic, dtype=jnp.float32))
+
+def dataload(cfg):
+    """ 
+    Returns the train and test dataset specified in the config
+    in the form of a pytorch dataloader
+    """
+
+    transform = transforms.Compose([FlattenAndCast()])
+    mnist_dataset_train = MNIST(cfg.dataset.path, download=True, transform=transform)
+    training_generator = NumpyLoader(mnist_dataset_train, batch_size=cfg.train_and_test.train.batch_size, shuffle=cfg.train_and_test.train.shuffle) # num_workers=mp.cpu_count()
+
+    mnist_dataset_test = MNIST(cfg.dataset.path, train=False, download=True, transform=transform)
+    test_generator = NumpyLoader(mnist_dataset_test, batch_size=cfg.train_and_test.test.batch_size, shuffle=cfg.train_and_test.test.shuffle) # num_workers=mp.cpu_count()
+
+    return training_generator, test_generator 
---
+++ main.py
@@ -0,0 +1,150 @@
+## Adding path (for some it is needed to import packages)
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+# Saving and loading
+import pickle
+
+# convret to torch for FID
+import torch
+
+## Jax
+
+# Stop jax from taking up 90% of GPU vram
+import os
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE']='false'
+#os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION']='0.5'
+#os.environ['XLA_PYTHON_CLIENT_ALLOCATOR']='platform'
+
+import jax
+import jax.numpy as jnp
+
+# Data
+from data import dataload 
+
+# TODO: Discuss this design choice in terms of the optimizer
+# maybe make this a seperate module
+
+# Model and optimizer
+from models.model import get_model 
+from optimizer.optimizers import get_optim
+
+# Loss
+from loss.loss import get_loss
+    
+# Visualization
+from visualization.visualize import display_images
+
+## Weights and biases
+import wandb
+
+# config mangement
+import hydra
+
+## Optimizer
+import optax
+
+### Train loop:
+
+# Gets config
+@hydra.main(config_path="configs/", config_name="defaults", version_base='1.3')
+def run_experiment(cfg):
+
+    # initialize Weights and Biases
+    print(cfg)
+    wandb.init(entity=cfg.wandb.setup.entity, project=cfg.wandb.setup.project)
+
+    # Get randomness key
+    key = jax.random.PRNGKey(cfg.model.key)
+    key, subkey = jax.random.split(key)
+
+    # Load train and test sets
+    train_dataset, test_dataset = dataload(cfg) 
+
+    # Get model forward call and its parameters
+    model_parameters, model_call = get_model(cfg, key = subkey) # model_call(x_in, timesteps, parameters)
+    #os.mkdir(wandb.run.dir)
+      
+    # Get optimizer and its parameters
+    optimizer, optim_parameters = get_optim(cfg, model_parameters)
+
+    # get sde
+    SDE = get_sde(cfg)
+
+    # get loss functions and convert to grad function
+    loss_fn = get_loss(cfg) # loss_fn(func, function_parameters, data, perturbed_data, time, key)
+
+    grad_fn = jax.grad(loss_fn,1) # TODO: try to JIT function partial(jax.jit,static_argnums=0)(jax.grad(loss_fn,1))
+
+    # start training for each epoch
+    for epoch in range(cfg.train_and_test.train.epochs): 
+        for i, (data, labels) in enumerate(train_dataset): # batch training
+
+            data = jax.device_put(data, sharding.reshape(n, 1))
+            # split key to keep randomness "random" for each training batch
+            key, *subkey = jax.random.split(key, 4)
+
+            # get timesteps given random key for this batch and data shape
+            timesteps = jax.random.uniform(subkey[0], (data.shape[0],), minval=0, maxval=1)
+
+            # Perturb the data with the timesteps trhough sampling sde trick (for speed, see paper for explanation)
+            perturbed_data = SDE.sample(timesteps, data, subkey[1])
+
+            # scale timesteps for more significance
+            scaled_timesteps = timesteps*999
+
+            # get grad for this batch
+              # loss_value, grads = jax.value_and_grad(loss_fn)(model_parameters, model_call, data, labels, t) # is this extra computation time
+            grads = grad_fn(model_call, model_parameters, data, perturbed_data, scaled_timesteps, subkey[2])
+
+            # get change in model_params and new optimizer params
+              # optim_parameters, model_parameters = optim_alg(optim_parameters, model_parameters, t_data, labels)
+            updates, optim_parameters = optimizer.update(grads, optim_parameters, model_parameters)
+
+            # update model params
+            model_parameters = optax.apply_updates(model_parameters, updates)
+
+            # Logging loss and an image
+            if i % cfg.wandb.log.frequency == 0:
+                  if cfg.wandb.log.loss:
+                    wandb.log({"loss": loss_fn(model_call, model_parameters, data, perturbed_data, scaled_timesteps, subkey[2])})
+                    # wandb.log({"loss": loss_value})
+                  if cfg.wandb.log.img:
+                     # dt0 = - 1/N
+                    drift = lambda t,y, args: SDE.reverse_drift(y, jnp.array([t]), args)
+                    diffusion = lambda t,y, args: SDE.reverse_diffusion(y, jnp.array([t]), args)
+                    get_sample = lambda t, key1, key0, xt: sample(0, 0, t.astype(float)[0], -1/1000, drift, diffusion, [model_call, model_parameters, key0], xt, key1) 
+                    key, *subkey = jax.random.split(key, len(perturbed_data)*2 + 1)
+
+                    args = (timesteps.reshape(-1,1), jnp.array(subkey[:len(subkey)//2]), jnp.array(subkey[len(subkey)//2:]), perturbed_data)
+
+                    images = jax.vmap(get_sample, (0, 0, 0, 0))(*args)
+
+                    # Rescale images for plotting
+                    mins, maxs=jnp.min(perturbed_data, axis=1).reshape(-1, 1), jnp.max(perturbed_data, axis=1).reshape(-1,1)
+                    rescaled_images = (perturbed_data-mins)/(maxs-mins)*255
+                    display_images(cfg, images, labels)
+                    display_images(cfg, perturbed_data, labels, log_title="Perturbed images")
+                    display_images(cfg, rescaled_images, labels, log_title="Min-Max Rescaled")
+                  if cfg.wandb.log.parameters:
+                          with open(os.path.join(wandb.run.dir, "paremeters.pickle"), 'wb') as f:
+                            pickle.dump((epoch*len(train_dataset) + i, model_parameters, optim_parameters), f, pickle.HIGHEST_PROTOCOL)
+                          wandb.save("paramters.pickle")
+                    #image = get_sample(timesteps[0], subkey[2], subkey[2], perturbed_data[0])
+
+                    #rescaled_perturbed = (perturbed_data[0]-jnp.min(perturbed_data[0]))/(jnp.max(perturbed_data[0])-jnp.min(perturbed_data[0]))*255
+
+                    #random_noise = jax.random.normal(subkey[2], data[0].shape)*255
+                    #image_from_random = get_sample(1, subkey[2], subkey[2], random_noise)
+
+                    #random_noise_uniform = jax.random.uniform(subkey[2], data[0].shape)*255
+                    #image_from_random_uniform = get_sample(1, subkey[2], subkey[2], random_noise_uniform)
+                    #display_images(cfg, [image, perturbed_data[0], data[0], rescaled_perturbed, image_from_random, random_noise,image_from_random_uniform, random_noise_uniform], ["sample", "perturbed", "original", "rescaled", "image from random", "random image", "uniform sample image", "uniform image"])
+
+                    #display_images(cfg, model_call(perturbed_data, scaled_timesteps, model_parameters, subkey[2]), labels)
+        # Test loop
+        if epoch % cfg.wandb.log.epoch_frequency == 0:
+            pass
+if __name__ == "__main__":
+    run_experiment()
